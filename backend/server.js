e569a4a495e50e85a016d58a35c6a6fbce057f61
test <eb2ray@gmail.com>
2020-10-27 15:21:20 +0200

    Added type: module to root so that we can use import instead of require

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
-const app = express();
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
 dotenv.config();
+
+const app = express();
 
 app.get('/', (req, res) => {
   res.send('API running...');
@@ -18,6 +19,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running  in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running  in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
